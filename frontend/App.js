2cf4f314b9f584d11d463c5539665556330fe682
vaalmigi <vaalmigi8@gmail.com>
2025-05-01 22:13:48 +0200

    Tilføjer fuld navigation setup til App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import LoginScreen from './screens/LoginScreen';
-import WorkoutScreen from './screens/WorkoutScreen';
-import GoalScreen from './screens/GoalScreen';
-import CalorieScreen from './screens/CalorieScreen';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import LoginScreen from "./screens/LoginScreen";
+import WorkoutScreen from "./screens/WorkoutScreen";
+import GoalScreen from "./screens/GoalScreen";
+import CalorieScreen from "./screens/CalorieScreen";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -42,8 +42,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
